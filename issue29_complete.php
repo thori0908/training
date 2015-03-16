2b1e8995b91eb4da238b385bd00d9ba7e16f0f07
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-03-16 17:52:24 +0900

    add a process of disconnect database in issue29_complete.php #29

--- issue29_complete.php
+++ issue29_complete.php
@@ -1,7 +1,7 @@
 <?php
 // connect database
 try {
-  $pdo = new PDO("mysql:dbname=issue28;host=127.0.0.1", "root");
+  $pdo = new PDO("mysql:dbname=issue28;host=127.0.0.1;charset=utf8", "root");
   $pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo 'Connection failed: ' . $e->getMessage();
@@ -13,14 +13,14 @@ try {
   $st->execute (array($_POST['prefecture']));
   $prefid = $st->fetch(PDO::FETCH_ASSOC);
 
-  $st1 = $pdo->prepare("INSERT INTO users (last_name, first_name, pref_id) VALUES (?, ?, ?)");
-  $st1->execute (array($_POST['lastname'], $_POST['firstname'], $prefid["pref_id"]));
+  $st1 = $pdo->prepare("INSERT INTO users (last_name, first_name, pref_id, email) VALUES (?, ?, ?, ?)");
+  $st1->execute (array($_POST['lastname'], $_POST['firstname'], $prefid["pref_id"], $_POST['mailaddress']));
 } catch (PDOException $e) {
   echo 'Connection failed2: ' . $e->getMessage();
 }
 
-
-
+// disconnect database
+$pdo = null;
 ?>
 
 <!DOCTYPE html>
