e1ce24f52471cb5dac5daadd06491ce6120c5e07
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-03-16 15:22:04 +0900

    modify issue29_complete.php #29

--- issue29_complete.php
+++ issue29_complete.php
@@ -1,16 +1,24 @@
 <?php
+// connect database
+try {
+  $pdo = new PDO("mysql:dbname=issue28;host=127.0.0.1", "root");
+  $pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo 'Connection failed: ' . $e->getMessage();
+}
 
-var_dump ($_POST);
-$pdo = new PDO("mysql:dbname=issue28;host=127.0.0.1", "root");
+// register users data
+try {
+  $st = $pdo->prepare ("select pref_id from prefectures where pref_name=?");
+  $st->execute (array($_POST['prefecture']));
+  $prefid = $st->fetch(PDO::FETCH_ASSOC);
 
-$st = $pdo->prepare("select pref_id from prefectures where pref_name='?'");
-$st->query(array($_POST['prefecture']));
-var_dump ($st);
+  $st1 = $pdo->prepare("INSERT INTO users (last_name, first_name, pref_id) VALUES (?, ?, ?)");
+  $st1->execute (array($_POST['lastname'], $_POST['firstname'], $prefid["pref_id"]));
+} catch (PDOException $e) {
+  echo 'Connection failed2: ' . $e->getMessage();
+}
 
-var_dump($_POST['prefecture']);
-var_dump($prefid);
-$st1 = $pdo->prepare("INSERT INTO users (last_name, first_name, pref_id, updated_at) VALUES (?, ?, ?, ?)");
-$st1->execute(array($_POST['lastname'], $_POST['firstname'], $prefid, $updated_at));
 
 
 ?>
