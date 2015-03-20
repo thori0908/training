5afc79be2b112028f68c53deef44eaf8677beff0
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-03-20 16:28:39 +0900

    modify added files #30

--- issue29_complete.php
+++ issue29_complete.php
@@ -21,19 +21,6 @@ try {
 
 // disconnect database
 $pdo = null;
+
+include 'issue30_complete.html.php';
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>issue17</title>
-</head>
-
-<body>
-  <h1>フォーム > 完了</h1>
-  <p>応募しました</p>
-  <a href="index.php">TOPページへ</a>
-  <p>Copyright 2014</p>
-</body>
-</html>
