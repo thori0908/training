7d58dc43532c35c9d486d4bf68647f15e18145f9
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-03-25 04:43:48 +0900

    modify User.php #30

--- User.php
+++ User.php
@@ -1,26 +1,26 @@
 <? php 
-class User
-{
-    //
+class User {
+
     private $formValues = array("lastname" => "", "firstname" => "", "gender" => "", "postcodeFirst" => "", "postcodeSecond" => "", 
                    "postcodeSecond " => "", "prefecture" => "", "postcode" => "", "mailaddress" => "", "other" => "",  
                    "opinion" => "", "hobbyMusic" => "", "hobbyMovie" => "", "hobbyOther" => "", "hobbyOtherText" => ""); 
-    
+
+    function __construct ($post, $session) {
     //空白処理・session更新-----------------------------
     foreach ($formValues as $key => $listValue) {
-        if (empty($_POST[$key])) {
-            $_SESSION[$key] = "";
-            $formValues[$key] = "";
+        if (empty($post[$key])) {
+        $session[$key] = "";
+        $formValues[$key] = "";
         } else { 
-            $_POST[$key] = mb_ereg_replace ('^[\s　]*(.*?)[\s　]*$', '\1', $_POST[$key]);//全角空白置換 
-            $formValues[$key] = trim($_POST[$key], " ");//空白処理 
-            $formValues[$key] = htmlspecialchars($_POST[$key]);  //htmlエスケープ処理
-            $_SESSION[$key] = $formValues[$key];//sessionの更新
+        $post[$key] = mb_ereg_replace ('^[\s　]*(.*?)[\s　]*$', '\1', $post[$key]);//全角空白置換 
+        $formValues[$key] = trim ($post[$key], " ");//空白処理 
+        $formValues[$key] = htmlspecialchars ($post[$key]);  //htmlエスケープ処理
+        $session[$key] = $formValues[$key];//sessionの更新
         }
     }
+    }
 
-    function getFormValue($key)
-    {
+    function getFormValue ($key) {
         $this->formValues $formValues[$key]; 
     }
 }
