9f396007b0913a1578c9269423d2f561380e63f2
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-04-22 17:29:49 +0900

    課題１の１をテスト

--- test_issue47/issue47_kadai1.php
+++ test_issue47/issue47_kadai1.php
@@ -1,1 +1,11 @@
 <?php
+
+require_once 'adder.php';
+
+class AdderTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdder() {
+        $adder = new Adder();
+        $this->assertEquals(3, $adder->add(1, 2));
+    }
+}
