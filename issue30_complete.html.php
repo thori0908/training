ba82ba69cf0623253eeee398268daf2fd2554e92
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-05-14 16:06:21 +0900

    フォントの強調修正 #20

--- issue30_complete.html.php
+++ issue30_complete.html.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <style type="text/css">
     h1 {font-size:30px; margin:10px; background-color:#009FFF}
+    #message {font-weight:bold;}
+
   </style>
   <title>issue17</title>
 </head>
@@ -13,7 +15,7 @@
     <h1>フォーム > 完了</h1>
   </header>
   <section>
-    <p style="text-shadow: 1px 1px 2px #000;">応募しました</p>
+    <p id="message">応募しました</p>
   </section>
   <footer>
     <a href="index.php">TOPページへ</a>
